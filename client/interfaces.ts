c3309a96ab95d0a8dafb2445a7eaba01764b8c2d
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2022-02-21 17:03:57 +0100

    [ADD] beginning of listing

---
+++ client/interfaces.ts
@@ -0,0 +1,5 @@
+export interface showsData {
+    images: {
+        poster: string
+    },
+}
--- client/pages/series/listing.tsx
+++ client/pages/series/listing.tsx
@@ -1,32 +1,40 @@
 import { CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
+import Header from "../Header";
+import { showsData } from "../../interfaces";
+const API_KEY: string | undefined = process.env.NEXT_PUBLIC_API_KEY;
+
+
 
 export default function SeriesListing() {
-    const [data, setData] = useState<Object[] | null>(null);
+    const [data, setData] = useState<showsData[]>([]);
 
     useEffect(() => {
-        fetch(`https://api.betaseries.com/shows/list?order=popularity`)
+        fetch(`https://api.betaseries.com/shows/list?order=popularity&client_id=${API_KEY}`)
             .then(res => res.json())
             .then(res => {
                 console.log(res);
-                setData(res)
+                if (res.shows) {
+                    setData(res.shows)
+                }
             })
             .catch(err => console.log(err));
     }, []);
 
-    if (data) {
-        return (
-            data.map((elt) => {
-                return (
-                    <div></div>
-                )
-            })
-        );
-    }
-    else {
-        return (
-            <CircularProgress />
-        );
-    }
+    return (
+        <div>
+            <Header />
+            {data.length > 0
+                ? <div className="series-wrapper">
+                    {data.length > 0 &&
+                        data.map(v => (
+                            <div className="series-card" style={{backgroundImage : `${v.images.poster}` }}>
+                                <img className="series-poster" src={v.images.poster} alt="" />
+                            </div>
+                        ))}
 
+                </div>
+                : <CircularProgress />}
+        </div>
+    );
 }
